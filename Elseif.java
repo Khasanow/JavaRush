1a9caac34b95b872b1eb20b85f72dcd8154845f8
hasanmishaalex@gmail.com <hasanmishaalex@gmail.com>
2022-01-12 14:55:00 +0300

    alseif

--- Elseif.java
+++ Elseif.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Elseif {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        int temp = s.nextInt();
+        int temp = s.nextInt(), a= s.nextInt(), b= s.nextInt();
 //        if (temp>20)
 //            System.out.println("надеть рубашку");
 //        else {
@@ -17,13 +17,31 @@ public class Elseif {
 //                }
 //            }
 //        }
-        if (temp>20)
-            System.out.println("надеть рубашку");
-        else
-            if (temp>10)
-                System.out.println("надеть свитер");
-            else
-                if
+
+//        if (temp>20)
+//            System.out.println("надеть рубашку");
+//        else
+//            if (temp>10)
+//                System.out.println("надеть свитер");
+//            else
+//                if (temp>0)
+//                    System.out.println("надеть плащ");
+//                else
+//                    System.out.println("надеть пальто");
+
+      if (temp>20)
+          System.out.println("надеть рубашку");
+      else if (temp>10)
+          System.out.println("надеть свитер");
+      else if (temp>0)
+          System.out.println("надеть плащ");
+      else
+          System.out.println("надеть пальто");
+
+      if (a<b)
+          System.out.println(a);
+      else
+          System.out.println(b);
 
     }
 }
